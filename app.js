9da2d9c0fc56f61523d98f14f2746cfc2e7dab03
amianki <ankita.mandal201@gmail.com>
2023-06-25 23:42:16 +0530

    all api created

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const port = 3000;
 const companies = [
     {
@@ -23,16 +24,53 @@ app.get('/companies', (req, res) => {
 });
 app.get('/company/:id', (req, res) => {
     let companyById = "Id does not exist!!";
-    for (let index = 0; index < companies.length; index++) {
-        if (companies[index].id == req.params.id) {
-            companyById = companies[index]; 
+    // for (let index = 0; index < companies.length; index++) {
+    //     if (companies[index].id == req.params.id) {
+    //         companyById = companies[index]; 
+    //     }
+    // }
+    companies.forEach(company => {
+        if (company.id == req.params.id) {
+            companyById = company;
         }
-    }
+    });
+
     res.send(companyById);
 
-
-
 });
+app.post('/company', (req, res) => {
+    companies.push(
+        {
+        id: 4567,
+        name: "infosys",
+        location: "bangalore"
+        }
+    )
+    
+    res.send("successfull");
+    
+});
+app.put('/company/:id',(req,res) =>{
+    let updatedCompany = {
+        id: req.body.id,
+        name: req.body.name,
+        location:req.body.location
+    }
+    for (let index = 0; index < companies.length; index++) {
+            if (companies[index].id == req.params.id) {
+                 companies[index] = updatedCompany; 
+            }
+        }
+    res.send(updatedCompany);
+})
+app.delete('/company/:id',(req,res) =>{
+    for (let index = 0; index < companies.length; index++) {
+        if (companies[index].id == req.params.id) {
+            companies.splice(index,1); 
+        }
+    }
+    res.send("deleted successfully..");
+})
 app.listen(port, () => {
     console.log("app is running on port no " + port);
 });
